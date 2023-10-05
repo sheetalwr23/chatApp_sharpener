dd466ac988b65ac477b1f4897a97c161b950eff3
Sheetal <Sheetalwr23@gmail.com>
2023-10-05 13:07:03 +0530

    task 4 login page frontend

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  jwt.verify(token, "your-secret-key", (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+    req.user = user;
+    next();
+  });
+};
